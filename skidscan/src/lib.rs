71afe4cf4f2c8f9fbdf597c55a0018243515a3bd
William Venner <william@venner.io>
2021-09-09 19:50:56 +0100

    Fix generic bounds for SigScan trait

--- skidscan/src/lib.rs
+++ skidscan/src/lib.rs
@@ -8,19 +8,9 @@ trait SigScan {
 	/// Scans this slice of bytes for a given signature
 	fn sigscan(&self, signature: &Signature) -> Option<SignatureRange>;
 }
-impl SigScan for dyn AsRef<[u8]> {
+impl<B: AsRef<[u8]>> SigScan for B {
 	fn sigscan(&self, signature: &Signature) -> Option<SignatureRange> {
 		signature.scan(self.as_ref())
-	}
-}
-impl SigScan for &[u8] {
-	fn sigscan(&self, signature: &Signature) -> Option<SignatureRange> {
-		signature.scan(self)
-	}
-}
-impl SigScan for Vec<u8> {
-	fn sigscan(&self, signature: &Signature) -> Option<SignatureRange> {
-		signature.scan(&self)
 	}
 }
 
